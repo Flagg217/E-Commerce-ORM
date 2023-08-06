935aba572890c4bac024f33d74edc29a5dda9253
Flagg217 <flagg217@gmail.com>
2023-08-06 16:53:05 -0700

    added get all product api route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,22 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { model: Category },
+        { model: Tag },
+      ],
+      order: [
+        ['id', 'DESC'],
+      ],
+    });
+    res.json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   // find all products
   // be sure to include its associated Category and Tag data
 });
