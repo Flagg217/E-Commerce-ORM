c7dd5a7e890ceb01b590034804301d0e930eee12
Flagg217 <flagg217@gmail.com>
2023-08-06 19:02:59 -0700

    fixed problem with product update put route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -43,30 +43,14 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', async (req, res) => {
-  tagIds = req.body.tagIds.map((tag_id) => {
-    return tag_id;
-  });
-  console.log(tagIds);
-  try {
-    const productData = await Product.create({
-      product_name: req.body.product_name,
-      price: req.body.price,
-      stock: req.body.stock,
-      category_id: req.body.category_id,
-      tag_ids: tagIds,
-    });
-    res.json(productData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
-      tagIds: [1, 2, 3, 4]
+      tagIds: [1, 2, 3, 4],
+      category_id: 7
     }
   */
   Product.create(req.body)
@@ -93,6 +77,7 @@ router.post('/', async (req, res) => {
 
 // update product
 router.put('/:id', (req, res) => {
+
   // update product data
   Product.update(req.body, {
     where: {
