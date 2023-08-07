46244c1b901f9e1d67c3739d57574ad5c6e59d48
Flagg217 <flagg217@gmail.com>
2023-08-06 19:34:51 -0700

    created put tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -49,7 +49,18 @@ router.post('/', async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
   // update a tag's name by its `id` value
 });
 
