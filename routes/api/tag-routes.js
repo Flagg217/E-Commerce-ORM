4e826391a61380247478b014588154e24d5c1aef
Flagg217 <flagg217@gmail.com>
2023-08-06 19:28:34 -0700

    created post tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,12 +22,30 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id,{
+      include: [
+        { model: Product },
+      ],
+    });
+    res.json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body);
+    res.json(tagData);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
   // create a new tag
 });
 
