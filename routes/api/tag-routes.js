23cc622e2668665edd3c2061b0371512be66a0af
Flagg217 <flagg217@gmail.com>
2023-08-06 19:40:36 -0700

    created delete tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -64,7 +64,17 @@ router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // delete on tag by its `id` value
 });
 
